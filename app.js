756ae9d9fe6e05557426b15851c85083ba859cd7
slithe <maxir2014@gmail.com>
2019-09-06 21:46:32 -0300

    newFunctions

---
+++ app.js
@@ -0,0 +1,89 @@
+window.onload = function () {
+
+  let firstValue
+  let selectedOperator
+  let secondValue
+
+  const display  = document.getElementById('display')
+  const btnMC    = document.getElementById('btnMC')
+  const btnMas   = document.getElementById('btnMas')
+  const btnMenos = document.getElementById('btnMenos')
+  const btnDiv   = document.getElementById('btnDiv')
+  const btnSeven = document.getElementById('btnSeven')
+  const btnEigth = document.getElementById('btnEigth')
+  const btnNine  = document.getElementById('btnNine')
+  const btnMult  = document.getElementById('btnMult')
+  const btnFour  = document.getElementById('btnFour')
+  const btnFive  = document.getElementById('btnFive')
+  const btnSix   = document.getElementById('btnSix')
+  const btnResta = document.getElementById('btnResta')
+  const btnOne   = document.getElementById('btnOne')
+  const btnTwo   = document.getElementById('btnTwo')
+  const btnThree = document.getElementById('btnThree')
+  const btnSuma  = document.getElementById('btnSuma')
+  const btnC     = document.getElementById('btnC')
+  const btn0     = document.getElementById('btn0')
+  const btnPunto = document.getElementById('btnPunto')
+  const btnEqual = document.getElementById('btnEqual')
+
+  const buttonOnClick = function(event){
+    display.innerText += event.target.textContent
+    //console.log(event.target.textContent)
+  }
+
+  const operatorFuntion = function(event){
+    firstValue = parseFloat(display.innerText)
+    selectedOperator = event.target.textContent
+    display.innerText=""
+    console.log("firstValue", firstValue)
+    console.log("selectedOperator", selectedOperator)
+  }
+
+  btnMC.onclick = buttonOnClick
+  btnMas.onclick = buttonOnClick
+  btnMenos.onclick = buttonOnClick
+  btnSeven.onclick = buttonOnClick
+  btnEigth.onclick = buttonOnClick
+  btnNine.onclick = buttonOnClick
+  btnFour.onclick = buttonOnClick
+  btnFive.onclick = buttonOnClick
+  btnSix.onclick = buttonOnClick
+
+  btnOne.onclick = buttonOnClick
+  btnTwo.onclick = buttonOnClick
+  btnThree.onclick = buttonOnClick
+
+  btnC.onclick = function(){
+    display.innerText = "";
+  }
+
+  btn0.onclick = buttonOnClick
+  btnPunto.onclick = buttonOnClick
+
+  btnMult.onclick = operatorFuntion
+  btnDiv.onclick = operatorFuntion
+  btnSuma.onclick = operatorFuntion
+  btnResta.onclick = operatorFuntion
+
+  btnEqual.onclick = function(){
+    let result
+    secondValue = parseFloat(display.innerText)
+    console.log(firstValue, secondValue)
+    switch (selectedOperator) {
+      case '+':
+        result = firstValue + secondValue
+        break;
+    case '-':
+        result = firstValue - secondValue
+        break;
+    case '*':
+        result = firstValue * secondValue
+        break;
+    case '/':
+        result = firstValue / secondValue
+        break;
+    default:
+    }
+    display.innerText = result
+  }
+}
